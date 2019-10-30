a1f1c598c8daa4d47130b4b11745022f6dccc124
avas27 <abhishekvermasn27@gmail.com>
2019-10-31 01:48:32 +0530

    home page built

--- JS/custom.js
+++ JS/custom.js
@@ -1,5 +1,22 @@
-var btn = document.getElementById("menuBut");
-btn.onclick = () => {
-  document.getElementById("navBg").classList.toggle("bg-light");
-  document.getElementById("mList").classList.toggle("mt-4");
-};
+// var btn = document.getElementById("menuBut");
+// btn.onclick = () => {
+//   document.getElementById("navBg").classList.toggle("bg-light");
+//   document.getElementById("mList").classList.toggle("mt-4");
+// };
+
+$(document).ready(function() {
+  $(".bgg").waypoint(
+    function(direction) {
+      if (direction == "down") {
+        $(".navbar").addClass("bg-light");
+        $(".navbar").css("box-shadow", "0 1px 7px black");
+      } else {
+        $(".navbar").removeClass("bg-light");
+        $(".navbar").css("box-shadow", "none");
+      }
+    },
+    {
+      offset: "250px"
+    }
+  );
+});
